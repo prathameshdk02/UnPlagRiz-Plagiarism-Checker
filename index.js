5c33277cd39112eb7b10d9e7be827244862f89ec
prathameshdk02 <prathameshdk02@gmail.com>
2023-01-13 22:10:03 +0530

    Testing Bro!

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const request = require('request');
+
+const app = express();
+
+app.get('/',(req,res,next)=>{
+    res.send("Yo Bakri");
+});
+
+app.get('/gettoken',(req,res)=>{
+    const reqHeader = {
+        'Content-Type': 'application/json'
+    };
+    let reqBody = `{
+        "email": "prathameshdk02@gmail.com",
+        "key": "9888c037-8fb1-44df-a969-ac4561b2d872"
+    }`;
+
+    let options = {
+        url:'https://id.copyleaks.com/v3/account/login/api',
+        method:'POST',
+        headers: reqHeader,
+        body: reqBody
+    };
+
+    request(options,(rErr,rRes,rBody)=>{
+        if(rRes.statusCode == 200){
+            let token = rRes.body.slice(rRes.body.indexOf(':')+2,rRes.body.indexOf(',')-1);
+            res.send(rRes);
+            console.log(token);
+        }else{
+            res.send(rErr);
+            console.log("Some error occurred retrieving the token.");
+        }
+    });
+});
+
+app.listen(3000);
